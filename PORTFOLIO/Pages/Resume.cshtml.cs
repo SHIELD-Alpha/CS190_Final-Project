91fe1b9c4c7c3b9065b645c4758d04997f111fad
Jayson Matsuura <jm74@stmarys-ca.edu>
2023-05-15 17:42:16 -0700

    first commit

---
+++ PORTFOLIO/Pages/Resume.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace PORTFOLIO.Pages
+{
+    public class ResumeModel : PageModel
+    {
+        private readonly ILogger<ResumeModel> _logger;
+
+        public ResumeModel(ILogger<ResumeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
